b98057f2303c06d45560c74e22c253c83ba2008b
yash <ymehta777@gmail.com>
2021-06-02 18:59:55 +0530

    :hammer: Minor Changes

--- src/ethereum/web3.js
+++ src/ethereum/web3.js
@@ -1,10 +1,8 @@
 import Web3 from "web3";
 let web3;
 
-if (typeof window !== "undefined") {
-  if (typeof window.ethereum !== "undefined") {
-    web3 = new Web3(window.ethereum);
-  }
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+  web3 = new Web3(window.ethereum);
 } else {
   web3 = new Web3(
     "https://kovan.infura.io/v3/98667df842b643c5a68077377ac327a2"
